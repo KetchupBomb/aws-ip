50b56221f1da1c7e05c89e76f8354bda0422e932
Brad King <king.me.brad@gmail.com>
2020-04-07 14:46:22 -0700

    Resolve hostname argument

--- aws.py
+++ aws.py
@@ -1,29 +1,34 @@
 #! /usr/bin/env python3
 
 from ipaddress import ip_interface, ip_address
-import json
 import requests
+import json
+import socket
 import sys
 
 
 URL = "https://ip-ranges.amazonaws.com/ip-ranges.json"
 
 
-def is_aws_ip(ip):
+def is_aws_ip(*ips):
+    ips = [ip_interface(ip) if "/" in ip else ip_address(ip) for ip in ips]
+
     aws = requests.get(URL).json()
     awsv4 = {ip_interface(a["ip_prefix"]): a for a in aws["prefixes"]}
     awsv6 = {ip_interface(a["ipv6_prefix"]): a for a in aws["ipv6_prefixes"]}
     aws = {**awsv4, **awsv6}
 
-    if "/" in ip:
-        ip = ip_interface(ip)
-    else:
-        ip = ip_address(ip)
+    result = []
+    for ip in ips:
+        for k, v in aws.items():
+            if ip in k.network:
+                result.append(v)
 
-    return [v for k, v in aws.items() if ip in k.network]
+    return result
 
 
 if __name__ == "__main__":
-    ret = is_aws_ip(sys.argv[1])
+    ips = [s[4][0] for s in socket.getaddrinfo(sys.argv[1], 0)]
+    ret = is_aws_ip(*ips)
     print(json.dumps(ret))
     exit(0 if ret else 1)
